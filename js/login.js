9f4df3a3eb40313dcd750b515926686e050514fb
kekons4 <kekons4@gmail.com>
2024-03-19 15:53:12 -0400

    added some MFA and TOTP first time setup aswell as for verified login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 const poolData = {
-    UserPoolId: 'us-east-1_tZvRzzyP1',
-    ClientId: '524k6humnvtabbla0s024icqa7'
+    UserPoolId: 'us-east-1_bfUX9IBS2',
+    ClientId: '6al7m2d7btkhf9ja7tmn0gb2gu'
 };
 
 let globalUserAttributes = {};
@@ -17,6 +17,8 @@ function signIn(username, password) {
         onSuccess: function (result) {
             console.log('Authentication successful');
             console.log('Access Token:', result.getAccessToken().getJwtToken());
+            
+            const accessToken = result.getAccessToken().getJwtToken();
             // Here you can redirect the user to another page or perform other actions
         },
         onFailure: function(err) {
@@ -40,7 +42,37 @@ function signIn(username, password) {
             delete userAttributes.email;
             // Complete the new password challenge
             cognitoUser.completeNewPasswordChallenge(newPassword, userAttributes, this);
+        },
+        mfaRequired: function(codeDeliveryDetails) {
+            // MFA is required to complete user authentication.
+            // Get the code from user and call
+            var verificationCode = prompt('Please input verification code ', '');
+            cognitoUser.sendMFACode(verificationCode, this);
+        },
+    
+        mfaSetup: function(challengeName, challengeParameters) {
+            // Handle MFA setup if necessary. This part is typically for NEW_PASSWORD_REQUIRED challenges
+            // or for setting up MFA on a user's account. It's uncommon for the initial login flow.
+            console.log("MFA SETUP");
+
+            cognitoUser.associateSoftwareToken(this);
+        },
+        associateSecretCode: async secretCode => {
+            console.log("SECRET CODE: ", secretCode);
+            //await this.setState({ QRCode: secretCode, showQRCode: true });
+            setTimeout(() => {
+              const challengeAnswer = prompt("Please input the TOTP code.", "");
+              cognitoUser.verifySoftwareToken(challengeAnswer, "My TOTP device", {
+                onSuccess: session => console.log("SUCCESS TOTP: ", session),
+                onFailure: err => console.error("ERROR TOTP: ", err)
+              });
+            }, 2000);
+        },
+        totpRequired: function(secretCode) {
+            var challengeAnswer = prompt("Please input the TOTP code.", "");
+            cognitoUser.sendMFACode(challengeAnswer, this, "SOFTWARE_TOKEN_MFA");
         }
+
     });
     
     // Call showNewPasswordForm() in your newPasswordRequired callback
